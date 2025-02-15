054f5dbd83b0fd4e884b455854824de8dc4e7869
mattbarcelona <mathieu.driencourt@yahoo.fr>
2025-02-15 17:29:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dbConnection = require("./config/config");
+const router = require("./routes/tasks");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+dbConnection();
+
+const PORT = 8080;
+app.listen(PORT, () =>
+  console.log(
+    `La aplicación está escuchando en el puerto http://localhost:${PORT}`
+  )
+);
